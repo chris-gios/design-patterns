1516778430493f26997aaace71654dca97df746f
Christian <christian.giraldo.1990@gmail.com>
2020-06-14 20:59:54 -0500

    added mediator

---
+++ src/main/java/behavioralPatterns/mediator/Usuario.java
@@ -0,0 +1,20 @@
+package behavioralPatterns.mediator;
+
+public class Usuario {
+	
+	private String nombre;
+	
+	public Usuario(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void recibirMensaje(String mensaje) {
+		System.out.println(mensaje);
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+
+}
