47aecc85eb0f7983b54747cc5bcc1a8507fea6be
Christian <christian.giraldo.1990@gmail.com>
2020-06-24 22:59:54 -0500

    jenkins test

--- src/main/java/structuralPatterns/adapter/Main.java
+++ src/main/java/structuralPatterns/adapter/Main.java
@@ -2,14 +2,27 @@ package structuralPatterns.adapter;
 
 public class Main {
 
+	/** Se crea la clase AdaptadorDB para que nos permita trabajar con SQL y NoSQL */
+
 	public static void main(String[] args) {
-		
-		IConexionSQL conexion = new AdaptadorDB( new ConexionMongoDB() );
-		
-		conexion.conexion();
-		
-		String resultado = conexion.runQuery();
+
+		/** Sin el adaptador no se pude conectar a un NoSQL */
+		IConexionSQL conexionSQL = new ConexionMySQL();
+
+		conexionSQL.conexion();
+
+		String resultadoSQL = conexionSQL.runQuery();
+		System.out.println(resultadoSQL);
+
+		/** Con el adaptador  si se puede conectar a un NoSQL */
+		IConexionSQL conexionNoSQL = new AdaptadorDB( new ConexionMongoDB() );
+
+		conexionNoSQL.conexion();
+
+		/** Este metodo ejecuta excecuteSentence de NoSQL */
+		String resultado = conexionNoSQL.runQuery();
 		System.out.println(resultado);
+
 
 	}
 
